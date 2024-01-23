27e5de0bc23570fe8584f096d970d0f9ee1b28e8
AlexConstandache <alex.constandache@layer-four.de>
2024-01-23 16:09:45 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:huskies_app/gui/views/auth/login_view.dart';
+import 'package:huskies_app/gui/views/widgets/navigation/nav_layer.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:huskies_app/logic/provider/notifier.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(
+    //* overrides: [], a list of Provider that can change on runtime, maybe same as a Flavor?
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final state = ref.read(appStateNotifierProvider);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: state.user != null && state.user!.isLogIn ? const ViewNavigator() : const LoginView(),
+    );
+  }
+}
